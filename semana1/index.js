3407981dde24f9159ac2f9e5e24f0492c4e0fcf5
Diego Flores <floresdiego2001xd@gmail.com>
2022-08-27 14:08:15 -0500

    desarrollo put-delete

--- semana1/index.js
+++ semana1/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request, response } from "express";
 
 //npm i nodemon -D --> Se utiliza para que el servidor se actualice
 const app = express();
@@ -19,7 +19,10 @@ app.get("/", (request, response) => {
 app.post("/create", function(req,res){
     //body almacena los que el usuario envia
     // destructuracion const { name, last_name} = req.body;
-    people.push(res.body);
+    const data = req.body;
+    data.id = people.length+1;
+    people.push(data);
+    //people.push(res.body);
     //El estatus 201 se envia si fue creado con exito
     //500 es para error (el servidor no respondes)
     //responder que no hay servicio 401 o 404 (no encuentra)
@@ -30,6 +33,59 @@ app.post("/create", function(req,res){
     });
 });
 //COMPLETAR EL PUT Y ELIMINAR
+//METODO PUT
+app.put("/update", (request,response)=>{
+
+    const data = request.body;
+
+ 
+    function actualizarDatosDeLaPersona(id,newNombre,newApellido,newEdad,newTelefono){
+        const personaEncontrada = people.find((person) =>{
+            return person.id === id;
+        });
+        
+        personaEncontrada["nombre"] = newNombre;
+        personaEncontrada["apellido"] = newApellido;
+        personaEncontrada["edad"] = newEdad;
+        personaEncontrada["telefono"] = newTelefono;
+    }
+
+    actualizarDatosDeLaPersona(data.id,data.nombre,data.apellido,data.edad,data.telefono)
+    //console.log(people)
+
+    return response.status(200).json({
+        ok:true,
+        data:"Persona actualizada con exito",
+        nuevosDatos: people
+    })
+
+})
+
+//METODO DELETE
+app.delete("/delete", (request, response) => {
+    const data = request.body;
+    
+   
+    function eliminarPersona(id){
+        //const index = people.findIndex((element) => element == id)
+        //index--> elimina segun el indice
+        //el segundo paràmetro del splice indica cuantos elementos eliminaremos, en este caso solo 1
+        people.forEach(function(person, index, object){
+            if(person.id == id){
+                object.splice(index,1);
+            }
+        })
+    }
+    eliminarPersona(data.id)
+
+    return response.status(200).json({
+        ok:true,
+        data:"Persona eliminasa satisfactoriamente",
+        datos: people
+    })
+})
+
+
 app.listen(6004, () => 
     console.log(`El servidor inicio en http://localhost:6004`)
 );
